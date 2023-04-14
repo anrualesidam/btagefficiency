b5dd3061be1e3cc132796ba46aa38d206261fd60
Daniel Ocampo-Henao <daniel.ocampoh@udea.edu.co>
2023-04-14 14:28:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
@@ -68,3 +69,11 @@ with eff:
 	ax.set(xlabel="$p_T$ [GeV]", ylabel="$|\eta|$", title=dataset)
 	
 	st.pyplot(fig)
+"""
+# Added to as an example of how to track your app using Umami
+import streamlit as st
+import streamlit.components.v1 as components
+
+components.html(f'<script async defer data-website-id="bd4bc597-839f-4f88-8c86-61b5785b8e03" src="https://umami-demo-production.up.railway.app/umami.js"></script>')
+
+st.write("Streamlit and Umami integration demo.")
